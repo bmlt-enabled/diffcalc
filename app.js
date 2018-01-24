96839de7de3bf3994ffbc5899096d2501d6d94a8
Danny Gershman <danny.gershman@gmail.com>
2018-01-23 23:08:53 -0500

    404 simple

--- app.js
+++ app.js
@@ -27,9 +27,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.status(404).send("No one lives here yet!");
 });
 
 // error handlers
